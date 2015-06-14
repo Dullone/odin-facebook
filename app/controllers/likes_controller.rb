51ea1d67ed3d93d93b1b25b4a93d498a22317624
dullone <rjippel@gmail.com>
2015-06-14 09:51:51 -0500

    Add unlike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,17 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @like = Like.new(user_id: current_user.id, post_id: params[:like][:post_id] )
     @like.save
+    redirect_to posts_path
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    if @like.user_id = current_user.id
+      @like.destroy
+    end
     redirect_to posts_path
   end
 end
